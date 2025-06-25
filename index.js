99c63fdae4ef7d7ab1855ffd453bf191a61b572f
techswarn <tech.swarns@gmail.com>
2025-06-25 11:40:36 +0530

    code update

---
+++ index.js
@@ -0,0 +1,28 @@
+// Import the framework and instantiate it
+import Fastify from 'fastify'
+const fastify = Fastify({
+  logger: true
+})
+
+// Declare a route
+fastify.get('/', async function handler (request, reply) {
+    const arr = [];
+
+    try{
+        while (true) {
+            arr.push(new Array(1e6).fill('*'));
+            console.log(`Allocated ${arr.length} million-element arrays`);
+          }
+    }catch(err){
+        return { hello: err }
+    }
+
+})
+
+// Run the server!
+try {
+  await fastify.listen({ port: 3000 })
+} catch (err) {
+  fastify.log.error(err)
+  process.exit(1)
+}
